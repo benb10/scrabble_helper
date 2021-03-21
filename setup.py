aed1b0d29ffd5622316647423521a54283a6f14e
benb10 <46840126+benb10@users.noreply.github.com>
2021-03-21 10:04:57 +1000

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="scrabble_helper",
+    version="0.1",
+    install_requires=[
+        "english_words>=1.0.3,<2",
+    ],
+    entry_points={}  # TODO.  SOmething like "console_scripts": ["scrabble = main:run"] ?
+)
